f3bd0d1412ebe828c1b8d8561ace8156e73bd7dc
ZAKUG04 <zambnefi@gmail.com>
2025-09-05 13:53:27 -0500

    add excersice 4

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,68 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        List<int> numbers = new List<int>();
+        int userNumber = -1;
+        while (userNumber != 0)
+        {
+            Console.Write("Enter a number (0 to quit): ");
+            string response = Console.ReadLine();
+            userNumber = int.Parse(response);
+
+            if (userNumber != 0)
+            {
+                numbers.Add(userNumber);
+            }
+        }
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+        Console.WriteLine($"The sum is: {sum}");
+
+        float average = ((float)sum) / numbers.Count;
+        Console.WriteLine($"The average is: {average}");
+
+        int max = numbers[0];
+
+        foreach (int number in numbers)
+        {
+            if (number > max)
+            {
+                max = number;
+            }
+        }
+        Console.WriteLine($"The largest number is: {max}");
+
+        int? smallestPositive = null;
+        foreach (int number in numbers)
+        {
+            if (number > 0)
+            {
+                if (smallestPositive == null || number < smallestPositive)
+                {
+                    smallestPositive = number;
+                }
+            }
+        }
+        if (smallestPositive != null)
+        {
+            Console.WriteLine($"The smallest positive number is: {smallestPositive}");
+        }
+        else
+        {
+            Console.WriteLine("There are no positive numbers.");
+        }
+        numbers.Sort();
+        Console.WriteLine("The sorted list is:");
+        foreach (int number in numbers)
+        {
+            Console.WriteLine(number);
+        } 
     }
 }
