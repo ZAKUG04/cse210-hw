8b9c16b2ff44cda8b65ca18834743ef9a2ed745e
ZAKUG04 <zambnefi@gmail.com>
2025-09-05 13:32:51 -0500

    game

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -5,5 +5,38 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        string playAgain = "yes";
+        while (playAgain.ToLower() == "yes")
+        {
+            Random randomGenerator = new Random();
+            int NumberMagic = randomGenerator.Next(1, 101);
+
+            int guess = -1;
+            int guessCount = 0;
+            while (guess != NumberMagic)
+            {
+                Console.Write("What is your guess? ");
+                guess = int.Parse(Console.ReadLine());
+                guessCount++;
+
+                if (NumberMagic > guess)
+                {
+                    Console.WriteLine("Geater");
+                }
+                else if (NumberMagic < guess)
+                {
+                    Console.WriteLine("lower");
+                }
+                else
+                {
+                    Console.WriteLine("You guessed it!");
+                    Console.WriteLine($"It took you {guessCount} guesses.");
+                }
+            }
+            
+            Console.Write("Do you want to play again? (yes/no): ");
+            playAgain = Console.ReadLine();
+        }
+        Console.WriteLine("Thanks for playing! Goodbye!");
     }
 }
